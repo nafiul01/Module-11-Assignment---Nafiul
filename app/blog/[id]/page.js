306c08de42b3ba50fa5c20d3bc2094bff0d14c78
nafiul01 <nafiul01@gmail.com>
2023-08-15 23:44:56 +0600

    Initial commit

---
+++ app/blog/[id]/page.js
@@ -0,0 +1,20 @@
+import getBlogPost from '@/lib/getBlogPost';
+import React from 'react';
+
+
+
+export default async function SingleBlog({params}){
+    
+    const id = params.id;
+    const post = await getBlogPost(id);
+
+    return (
+        <section>
+            <h1>{post.title}</h1>
+            <div>
+                {post.body}
+            </div>
+        </section>
+    );
+
+};
